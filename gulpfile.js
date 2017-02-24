9f934d141f98ad8f0922800e211d64c091196e50
Octavian Rotari <octavianrotari@gmail.com>
2017-02-24 19:19:01 +0000

    added image and process it with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,72 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var cleanCss = require('gulp-clean-css');
+var server = require('gulp-server-livereload');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
+var imageMin = require('gulp-imagemin');
+var imageResize = require('gulp-image-resize');
+var debug = require('gulp-debug');
 
 gulp.task('sass', function () {
-  gulp.src('src/scss/*.scss')
+  gulp.src('src/resources/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('src/tmp/'));
 });
 
-gulp.task('styles', function () {
-  gulp.src('dist/css/*.css')
-  .pipe(cleanCss({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('dist/css'));
+gulp.task('styles', [ 'sass' ], function () {
+  var stream = gulp.src('src/**/*.css')
+    .pipe(cleanCss({ compatibility: 'ie8' }))
+    .pipe(debug({ title: 'styles:' }))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('dist/css/'));
+  return stream;
+});
+
+gulp.task('imageMin', function () {
+  gulp.src('src/resources/img/*')
+    .pipe(imageMin())
+    .pipe(gulp.dest('src/resources/img/'));
+});
+
+var resizeImageTaskNames = [];
+
+[ 2340, 1170 ].forEach(function (size) {
+  var imageSize = 'imageResize_' + size;
+  gulp.task(imageSize, function () {
+    gulp.src('src/resources/img/code.jpeg')
+      .pipe(imageResize({
+        width: size,
+        height: size,
+        quality: 0.5
+      }))
+      .pipe(rename( function (path) {
+        path.basename += size;
+      }))
+      .pipe(gulp.dest('dist/img/'));
+  });
+  resizeImageTaskNames.push(imageSize);
+});
+
+gulp.task('imageResize', resizeImageTaskNames);
+
+gulp.task('cleanImg', function () {
+  gulp.src('dist/img/', { read: false })
+    .pipe(clean());
 });
 
 gulp.task('watch', function () {
-  gulp.watch('src/scss/*.scss', [ 'sass', 'styles' ]);
+  gulp.watch([ 'src/resources/scss/*.scss', 'src/vendors/css/*.css' ], [ 'sass', 'styles' ]);
 });
 
-gulp.task('default', [ 'sass', 'styles', 'watch' ]);
+gulp.task('server', function () {
+  gulp.src('./')
+    .pipe(server({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+});
+
+gulp.task('default', [ 'cleanImg', 'imageMin', 'imageResize', 'sass', 'styles', 'watch', 'server' ]);
